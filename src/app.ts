aa208feb92d1f2a2502731f33b676a669dd07513
Yash Sharma <yashsharma.aiml@gmail.com>
2026-02-02 19:40:26 +0530

    Connected routes and middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import studentRoutes from './routes/student.routes';
+import { errorHandler } from './middlewares/error.middleware';
 
 const app: Application = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Basic Route for testing
+app.use('/api/students', studentRoutes);
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome to SESD CRUD Backend' });
 });
+
+app.use(errorHandler);
 
 export default app;
